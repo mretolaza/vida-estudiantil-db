8e68b7e325e46e396e5d19e4ea694233874b49bb
Maria Vasquez <maria.ines.vf@gmail.com>
2020-04-26 18:45:08 -0600

    primeras 3 relaciones finalizadas

--- vidaestudiantil.sql
+++ vidaestudiantil.sql
@@ -1,3 +1,40 @@
+drop table if exists persona;
+create table persona(
+	id INT primary key,
+	contrasena varchar(30) not null,
+	horas_beca INT,
+	carrera varchar(100),
+	correo varchar(100),
+	carne INT,
+	genero varchar(30) not null,
+	nombre varchar(100) not null,
+	facultad varchar(100),
+	apellido varchar(50)
+);
+
+drop table if exists tipo;
+create table tipo(
+	id_tipo INT primary key,
+	tipo varchar(30) not null
+);
+
+
+drop table if exists taller;
+create table taller(
+	id_taller INT primary key,
+	nombre varchar(50) not null,
+	salon varchar(10),
+	fecha DATE not null,
+	descripcion varchar(100) not null
+);
+
+drop table if exists equipo;
+create table equipo(
+	id_equipo INT primary key,
+	nombre varchar(50) not null,
+	deporte varchar(50) not null
+);
+
 drop table if exists es;
 create table es (
 	id_es int primary key,
